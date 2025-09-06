210d9cad2ae9ed3c69bfa7a982f7de1bd748d55c
Fedoraard <fedora.ariefa07@upi.edu>
2025-09-06 20:44:44 +0700

    Add files via upload

---
+++ website kopi donqi/js/script.js
@@ -0,0 +1,8 @@
+//toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+
